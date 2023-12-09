45ba1467172689fd3c77f417a3b834b49c20cbf7
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2023-12-09 14:12:16 +0000

    Sat, Dec 9, 2023, 7:42 PM +05:30

--- fuel.py
+++ fuel.py
@@ -1,9 +1,9 @@
 def main():
     x = get_fraction()
     x = round(x)
-    if(x==100):
+    if(x==100 or x==99):
         print("F")
-    elif(x==0):
+    elif(x==0 or x==1):
         print("E")
     else:
         print(f"{x}%",end="")
@@ -15,13 +15,12 @@ def get_fraction():
             array = (f.partition("/"))
             n = array[0]
             d = array[2]
-            # print(n)
-            # print(d)
-            if isinstance(n,float):
-                continue
-            if isinstance(n,float):
-                continue
-            return (int(n)/int(d))*100
+            if(int(n)<=int(d)):
+                if isinstance(n,float):
+                    continue
+                if isinstance(n,float):
+                    continue
+                return (int(n)/int(d))*100
         except ValueError:
             pass
         except ZeroDivisionError:
