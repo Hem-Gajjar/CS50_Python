f0eb283f93f3b8496d1d42680429b1b1940a0820
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2023-12-09 07:35:04 +0000

    Sat, Dec 9, 2023, 1:05 PM +05:30

---
+++ coke.py
@@ -0,0 +1,17 @@
+
+
+price = 50
+amount=0
+while True:
+    coin = int(input("Insert Coin: "))
+    if coin == 25:
+        amount += 25
+    elif coin == 10:
+        amount +=10
+    elif coin == 5:
+        amount +=5
+
+    print("Amount Due:",price-amount)
+    if amount == price:
+        break
+
