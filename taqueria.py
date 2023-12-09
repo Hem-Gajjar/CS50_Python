86142228c3e35cb19108654a00502e99345a2389
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2023-12-09 14:36:06 +0000

    Sat, Dec 9, 2023, 8:06 PM +05:30

--- taqueria.py
+++ taqueria.py
@@ -18,7 +18,7 @@ while True:
         for i in menu:
             if i.lower() == item.lower():
                 sum  += float(menu[i])
-                print(f"Total: ${sum}")
+                print(f"Total: ${:02d}".format(sum))
     except EOFError:
         print()
         break
