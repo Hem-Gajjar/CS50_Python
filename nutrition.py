3448cb9b7f7df5b2167fc0abea60c190475eed96
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2023-12-09 10:42:36 +0000

    Sat, Dec 9, 2023, 4:12 PM +05:30

--- nutrition.py
+++ nutrition.py
@@ -2,8 +2,10 @@ def main():
     food = input("Item: ")
     table = [
         {"Name":"apple","Calories":"130"},
-        {"Name":"avacado","Calories":"50"},
+        {"Name":"avocado","Calories":"50"},
         {"Name":"banana","Calories":"110"},
+        {"Name":"Kiwifruit","Calories":"90"},
+        {"Name":"pear","Calories":"100"},
         {"Name":"sweet cherries","Calories":"100"}
     ]
 
