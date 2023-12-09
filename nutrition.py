d603c5c8a6758a818017dc56ca458d4bdb3d728f
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2023-12-09 10:40:18 +0000

    Sat, Dec 9, 2023, 4:10 PM +05:30

--- nutrition.py
+++ nutrition.py
@@ -1,8 +1,16 @@
-food = input("Item: ")
 def main():
+    food = input("Item: ")
+    table = [
+        {"Name":"apple","Calories":"130"},
+        {"Name":"avacado","Calories":"50"},
+        {"Name":"banana","Calories":"110"},
+        {"Name":"sweet cherries","Calories":"100"}
+    ]
+
+    for x in table:
+        if(x["Name"] == food.lower()):
+            print("Calories:",x["Calories"])
 
 
+main()
 
-
-def get_calories():
-    
