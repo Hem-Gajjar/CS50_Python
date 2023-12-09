beab9555cd3b06d8e5a0ac9f6b779213578ae4f5
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2023-12-09 09:58:59 +0000

    Sat, Dec 9, 2023, 3:28 PM +05:30

--- plates.py
+++ plates.py
@@ -23,7 +23,7 @@ def is_valid(s):
 
 
 
-    if not(s.isalnum()):
+    if s.isalpha():
          if(i == len(s)-1):
             alphabets = s[0:index_where_num_start]
             numbers = s[index_where_num_start:len(s)]
