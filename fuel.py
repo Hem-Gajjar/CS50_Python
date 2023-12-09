c66c1195b47e70ff657b2e8cde8a39063366f936
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2023-12-09 13:13:24 +0000

    Sat, Dec 9, 2023, 6:43 PM +05:30

--- fuel.py
+++ fuel.py
@@ -1,10 +1,17 @@
 def main():
-    x = get_fraction("Fraction: ")
-
-def get_fraction(prompt):
+    x = get_fraction()
+    print(x)
+def get_fraction():
     while True:
         try:
-           
+            f = input("Fraction: ")
+            n = f[0]
+            d = f[2]
+            print(n)
+            print(d)
+            x = int(n)/int(d)
+            # print(x)
+            return (x*100)
         except ValueError:
             pass
         except ZeroDivisionError:
