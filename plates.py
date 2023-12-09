5dbc9bcb359f26f1bdab3bbb10d96f6502be82f7
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2023-12-09 08:48:30 +0000

    Sat, Dec 9, 2023, 2:18 PM +05:30

---
+++ plates.py
@@ -0,0 +1,18 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+
+    length = len(s)
+    if 2 < length < 6
+        return False
+    else
+        if 
+
+
+main()
