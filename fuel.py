1b83a0127c0b6f625cbcb90623a7d8c32ce72c8b
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2023-12-09 13:45:53 +0000

    Sat, Dec 9, 2023, 7:15 PM +05:30

--- fuel.py
+++ fuel.py
@@ -18,7 +18,7 @@ def get_fraction():
             print(d)
             if n.isdecimal():
                 continue
-            
+            return n/d
         except ValueError:
             pass
         except ZeroDivisionError:
