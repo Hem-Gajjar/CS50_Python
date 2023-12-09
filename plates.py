5b789b6212787971972bef6839b23b94f879236f
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2023-12-09 09:09:03 +0000

    Sat, Dec 9, 2023, 2:39 PM +05:30

--- plates.py
+++ plates.py
@@ -7,25 +7,58 @@ def main():
 
 
 def is_valid(s):
-    i = 0
-    flag = True
+
     length = len(s)
-    if length < 2 or length > 6:
-        flag = False
-    else:
-        flag = True
-    if s[0].isnumeric() or s[1].isnumeric():
-        flag =  False
-    else:
+
+    if length >= 2 or length <= 6
         
-        while s[i].isalpha():
-            i+=1
-        i+=1
-        while s[i].isnumeric():
-            i+=1
-        if(i >= len(s)):
-            flag = False
-        else:
-            flag = True
-    return flag
+    else:
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # i = 0
+    # flag = True
+    # length = len(s)
+    # if length < 2 or length > 6:
+    #     flag = False
+    # else:
+    #     flag = True
+    # if s[0].isnumeric() or s[1].isnumeric():
+    #     flag =  False
+    # else:
+
+    #     while s[i].isalpha():
+    #         i+=1
+    #     i+=1
+    #     while s[i].isnumeric():
+    #         i+=1
+    #     if(i >= len(s)):
+    #         flag = False
+    #     else:
+    #         flag = True
+    # return flag
 main()
