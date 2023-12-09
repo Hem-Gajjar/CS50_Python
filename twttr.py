e94cd5f84af91dc5db2d94103f9488a4168d9325
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2023-12-09 08:19:16 +0000

    Sat, Dec 9, 2023, 1:49 PM +05:30

--- twttr.py
+++ twttr.py
@@ -1,10 +1,14 @@
 str = ("Input: ")
 vowels = ["a","e","i","o","u"]
+i=0
+j=0
 while i < len(str):
+    j=0
     while j < len(vowels)
         if str[i].lower == vowels[j]:
             print("",end="")
         else:
             print(str[i],end="")
-    
+        j+=1
+    i+=1
 
