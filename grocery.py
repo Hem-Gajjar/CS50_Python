2b333bfb015929f80a943ac9cbabe1b8ac631bb6
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-01-29 09:35:20 +0000

    Mon, Jan 29, 2024, 3:05 PM +05:30

--- grocery.py
+++ grocery.py
@@ -1,9 +1,23 @@
-vegies = {}
+vegies = {
+
+
+}
+
+def count_item(x):
+    i=0
+    count=0
+    while i < len(vegies):
+        if list(vegies[i]) == x :
+            count=count+1
+        i = i+1
+    return count
+
+
 while True:
     try:
         x = input()
-        
-        vegies.update({x:0})
+        count = count_item(x)
+        vegies.update({x:count})
     except EOFError:
         break
 print(vegies)
