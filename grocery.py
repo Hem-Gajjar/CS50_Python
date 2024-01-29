e7cf06e91f3307a163408c9206608bfc825369a8
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-01-29 09:23:53 +0000

    Mon, Jan 29, 2024, 2:53 PM +05:30

--- grocery.py
+++ grocery.py
@@ -1,8 +1,11 @@
-vegies = {name:'',count}
+vegies = {
+    "name":"",
+    "count":0
+}
 while True:
     try:
         x = input()
-        
+        vegies.update("name:")
     except EOFError:
         break
 print(vegies)
