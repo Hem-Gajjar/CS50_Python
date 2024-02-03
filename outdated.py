211375cbe36b311c9d7ca3c727567e756f10aec5
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-03 19:59:27 +0000

    Sun, Feb 4, 2024, 1:29 AM +05:30

--- outdated.py
+++ outdated.py
@@ -1,4 +1,19 @@
 
+# month_list = [
+#     "January",
+#     "February",
+#     "March",
+#     "April",
+#     "May",
+#     "June",
+#     "July",
+#     "August",
+#     "September",
+#     "October",
+#     "November",
+#     "December"
+# ]
+
 input_date = input("Date: ")
 month,day,year = input_date.split('/')
 
