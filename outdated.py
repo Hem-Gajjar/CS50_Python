4c70667d71ca64a0f86ce59f87dc5f5bcea233fe
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-03 20:10:20 +0000

    Sun, Feb 4, 2024, 1:40 AM +05:30

--- outdated.py
+++ outdated.py
@@ -13,17 +13,20 @@ month_list = [
     "November",
     "December"
 ]
-
-input_date = input("Date: ")
-month,day,year = input_date.split('/')
-
-
-
-
-if(type(month) == int):
+# while True:
+try:
+    input_date = input("Date: ")
+    month,day,year = input_date.split('/')
     month = int(month)
     day = int(day)
     print(f"{year:02}-{month:02}-{day:02}")
-else:
-    month = int(month_list.index(month))
+except ValueError:
+    month = month_list.index(month)
     print(month)
+
+
+
+# if(type(month) == int):
+
+# else:
+
