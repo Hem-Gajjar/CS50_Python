0137c60b488e40165f8407a73db2fe81c245bf0f
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-03 20:03:59 +0000

    Sun, Feb 4, 2024, 1:33 AM +05:30

--- outdated.py
+++ outdated.py
@@ -1,23 +1,27 @@
 
-# month_list = [
-#     "January",
-#     "February",
-#     "March",
-#     "April",
-#     "May",
-#     "June",
-#     "July",
-#     "August",
-#     "September",
-#     "October",
-#     "November",
-#     "December"
-# ]
+month_list = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
 
 input_date = input("Date: ")
 month,day,year = input_date.split('/')
 
-month = int(month)
-day = int(day)
 
-print(f"{year:02}/{month:02}/{day:02}")
+
+
+if(type(month) == int):
+    month = int(month)
+    day = int(day)
+    print(f"{year:02}-{month:02}-{day:02}")
+else:
