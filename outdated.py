ee00eac1cf6b70c28506a3a5f21302b2669f4e51
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-03 19:56:57 +0000

    Sun, Feb 4, 2024, 1:26 AM +05:30

--- outdated.py
+++ outdated.py
@@ -1,6 +1,10 @@
 
 input_date = input("Date: ")
 month,day,year = input_date.split('/')
+
+month = int(month)
+day = int(day)
+
 print(f"Month::{month:02}")
 print(f"Day::{day:02}")
 print(f"Year::{year:02}")
