a7c3c946f72fbd3313b256bacabd7c700db5fff1
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-04 20:24:36 +0000

    Mon, Feb 5, 2024, 1:54 AM +05:30

--- figlet.py
+++ figlet.py
@@ -1,10 +1,14 @@
 from pyfiglet import Figlet
 import sys
 
-if len(sys.argv)==
 
 
-text = "Demo"
-f = Figlet(font='slant')
-print(f.renderText(text))
+if len(sys.argv)==0
+    text = input("Input: ")
+    f = Figlet(font='slant')
+    print(f.renderText(text))
+
+
+
+
 
