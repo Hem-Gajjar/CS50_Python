4cadcc141135050c8db0e6ff935d63252d0128a4
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-04 20:33:02 +0000

    Mon, Feb 5, 2024, 2:03 AM +05:30

--- figlet.py
+++ figlet.py
@@ -8,8 +8,7 @@ if len(sys.argv)==1:
     f = Figlet(font='banner3')
     print("Output:")
     print(f.renderText(text))
-
-if len(sys.argv)==3:
+elif len(sys.argv)==3:
     font_cmd = sys.argv[1]
     font_name = sys.argv[2]
     if(font_cmd == "-f" or font_cmd == "--font"):
@@ -17,7 +16,8 @@ if len(sys.argv)==3:
         f = Figlet(font=font_name)
         print("Output:")
         print(f.renderText(text))
-
+else:
+    sys.exit()
 
 
 
