af1a916f6c6fe6d8610de0418655e9621e0057f8
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-04 20:26:20 +0000

    Mon, Feb 5, 2024, 1:56 AM +05:30

--- figlet.py
+++ figlet.py
@@ -3,9 +3,9 @@ import sys
 
 
 
-if len(sys.argv)==0
+if len(sys.argv)==0:
     text = input("Input: ")
-    f = Figlet(font='slant')
+    f = Figlet(font='banner3')
     print(f.renderText(text))
 
 
