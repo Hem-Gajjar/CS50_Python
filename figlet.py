7045dfc85fe708d649115017f7019baaf4cb4943
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-04 20:56:52 +0000

    Mon, Feb 5, 2024, 2:26 AM +05:30

--- figlet.py
+++ figlet.py
@@ -11,18 +11,21 @@ if len(sys.argv)==1:
 elif len(sys.argv)==3:
     font_cmd = sys.argv[1]
     font_name = sys.argv[2]
-    if(all_fonts.index(font_name)):
-        if(font_cmd == "-f" or font_cmd == "--font"):
-            text = input("Input: ")
-            try:
-                f = Figlet(font=font_name)
-            except:
+    try:
+        if(all_fonts.index(font_name)):
+            if(font_cmd == "-f" or font_cmd == "--font"):
+                text = input("Input: ")
+                try:
+                    f = Figlet(font=font_name)
+                except:
+                    sys.exit("Invalid usage")
+                print("Output:")
+                print(f.renderText(text))
+            else:
                 sys.exit("Invalid usage")
-            print("Output:")
-            print(f.renderText(text))
         else:
             sys.exit("Invalid usage")
-    else:
+    except ValueError:
         sys.exit("Invalid usage")
 else:
     sys.exit("Invalid usage")
