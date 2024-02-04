a23fe5c36c9eade1b4c11ab4197d6a07f6dff208
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-04 20:30:40 +0000

    Mon, Feb 5, 2024, 2:00 AM +05:30

--- figlet.py
+++ figlet.py
@@ -8,6 +8,14 @@ if len(sys.argv)==1:
     f = Figlet(font='banner3')
     print(f.renderText(text))
 
+if len(sys.argv)==3:
+    font_cmd = sys.argv[1]
+    font_name = sys.argv[2]
+    if(font_cmd == "-f" or font_cmd == "--font"):
+        text = input("Input: ")
+        f = Figlet(font=font_name)
+        print(f.renderText(text))
+
 
 
 
