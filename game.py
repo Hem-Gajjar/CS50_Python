96351b59cf3ff58af8a25aa54f2212c885c7f8ea
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-05 11:29:01 +0000

    Mon, Feb 5, 2024, 4:59 PM +05:30

--- game.py
+++ game.py
@@ -9,7 +9,9 @@ def is_neg(x):
 while True:
     try:
         level = input("Level: ")
-        if(is_neg(level) and level.isalpha()):
+        if(level.isalpha() or level.isalnum()):
+            continue
+        if(is_neg(level)):
             continue
         else:
             level = int(level)
