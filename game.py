a4766c17b00bbdac43828225302fc9bff028fe9a
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-05 11:13:47 +0000

    Mon, Feb 5, 2024, 4:43 PM +05:30

--- game.py
+++ game.py
@@ -2,13 +2,20 @@ import random
 while True:
     try:
         level = int(input("Level: "))
-        if(level < 0):
+        if(is_neg(level)):
             continue
         else:
-            random.randint(1,level)
-
+            rand = random.randint(1,level)
+            guess = int(input("Guess: "))
+            if(rand == guess):
+                print("Just right!")
     except:
         break
 
 
-def is_negetive
+def is_neg(x):
+    print("called")
+    if (x<0):
+        return True
+    else:
+        return False
