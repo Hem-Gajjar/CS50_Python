522accecdc07d00786bcbbb0117f8247b7e2ed0c
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-05 13:42:39 +0000

    Mon, Feb 5, 2024, 7:12 PM +05:30

--- game.py
+++ game.py
@@ -23,6 +23,8 @@ while True:
                         break
                     elif(rand<guess):
                         print("Too large!")
+                        if(guess > level):
+                            break
                         continue
                     elif(rand>guess):
                         print("Too small!")
