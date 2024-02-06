2b76d5b1ae686b3c54426053c21fc97412532ba3
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-06 11:47:16 +0000

    Tue, Feb 6, 2024, 5:17 PM +05:30

--- bitcoin.py
+++ bitcoin.py
@@ -7,14 +7,14 @@ try:
         response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 
         o = response.json()
-        for i in o["bpi"]:
-            for j in i[0]:
-                print(j[2])
-
+        # for i in o[3]:
+        #     for j in i[0]:
+        #         print(j[2])
+        print(o)
     elif(len(sys.argv)==1):
         print("Missing command-line argument")
         sys.exit()
-        
+
 except ValueError:
     print("Command-line argument is not a number")
     sys.exit()
