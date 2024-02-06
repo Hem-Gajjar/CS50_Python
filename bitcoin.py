22e32355279798709ac10d2d78c450edc541df34
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-06 12:01:20 +0000

    Tue, Feb 6, 2024, 5:31 PM +05:30

--- bitcoin.py
+++ bitcoin.py
@@ -3,8 +3,9 @@ import requests
 import json
 try:
     if(len(sys.argv)==2):
-        print("hhh")
-        num = float(sys.argv[1])
+        print(sys.argv[1])
+
+        num = sys.argv[1]
 
         response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 
