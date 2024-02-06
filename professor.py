e11726854dd2e910ff57ffb784a206a152ec6467
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-06 08:10:17 +0000

    Tue, Feb 6, 2024, 1:40 PM +05:30

--- professor.py
+++ professor.py
@@ -1,15 +1,28 @@
 import random
 
 def main():
-    
-
+    level = get_level()
+    i=0
+    while(i<10):
+        
 
 def get_level():
     while True:
         try:
             x = int(input("Level: "))
-            return(x)
+            if(x >= 1 and x <= 3 ):
+                return(x)
+            else:
+                pass
         except ValueError:
+            pass
+
+def generate_integer():
+    while True:
+        x = random.randint()
+        if(x >= 0):
+            return x
+        else:
             pass
 
 if __name__ == "__main__":
