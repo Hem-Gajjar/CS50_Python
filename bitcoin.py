280a393187f76e36a680e25d5c53b55420aac11f
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-06 11:10:45 +0000

    Tue, Feb 6, 2024, 4:40 PM +05:30

--- bitcoin.py
+++ bitcoin.py
@@ -1,5 +1,12 @@
 import sys
 import requests
 
-num = float(input(requests.argv()))
+try:
+    if(len(sys.argv())!=2):
+        num = float(input(sys.argv()))
+        
+    elif(len(sys.argv())==1):
+        print("Missing command-line argument")
+except ValueError:
+    print("Command-line argument is not a number")
 
