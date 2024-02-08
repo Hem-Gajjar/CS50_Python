28d92bf706a0df8d4051d16aee825ca2aee1c905
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-08 13:26:02 +0000

    Thu, Feb 8, 2024, 6:56 PM +05:30

--- twttr.py
+++ twttr.py
@@ -7,15 +7,17 @@ def check(str):
     i=0
     j=0
     while i < len(str):
-    j=0
-    flag = False
-    while j < len(vowels):
-        if str[i].lower() == vowels[j]:
-            flag = True
-            break
-        j+=1
-    if flag==False:
-        print(str[i],end="")
-    i+=1
-    
+        j=0
+        flag = False
+        while j < len(vowels):
+            if str[i].lower() == vowels[j]:
+                flag = True
+                break
+            j+=1
+        if flag==False:
+            print(str[i],end="")
+        i+=1
 
+    print(str)
+
+check(str)
