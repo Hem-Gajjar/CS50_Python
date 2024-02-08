69bba72c857cd30bdb92fa293902a7378f008259
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-08 13:38:37 +0000

    Thu, Feb 8, 2024, 7:08 PM +05:30

--- twttr_new.py
+++ twttr_new.py
@@ -1,5 +1,17 @@
-str = input("Input: ")
-xyz = "hello"
-for i in str:
-    xyz.append(i)
+input_str = input("Input: ")
+vowels = ["a","e","i","o","u"]
 
+def check(str):
+    xyz = ""
+    str = str.lower()
+    for i in str:
+        for x in vowels:
+            if(i==x):
+                break
+            else:
+                xyz+=i
+                break
+
+    return xyz
+
+print(check(input_str))
