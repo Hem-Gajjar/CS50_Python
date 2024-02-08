77f4ba2767efac2c1dcae170f989014510f4d41b
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-08 14:21:13 +0000

    Thu, Feb 8, 2024, 7:51 PM +05:30

--- test_twttr.py
+++ test_twttr.py
@@ -1,8 +1,14 @@
 from twttr import shorten
 
-def test_twttr():
+def test_normal():
     assert shorten("twitter") == "twttr"
     assert shorten("hem") == "hm"
+
+def test_capital():
     assert shorten("hEm") == "hm"
+
+def test_number():
     assert shorten("h3m") == "hm"
+
+def test_pun():
     assert shorten("h,m") == "hm"
