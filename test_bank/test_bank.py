c76f9b8af1ef2ba55891245ef7f351efdfc81510
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-10 07:06:35 +0000

    Sat, Feb 10, 2024, 12:36 PM +05:30

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,4 +1,20 @@
 from bank import value
 
+def main():
+    test_hello()
+    test_hii()
+    test_namaste()
+
+
 def test_hello():
-    assert value("Hello") == 100
+    assert value("Hello") == 0
+    assert value("hello") == 0
+
+def test_hii():
+    assert value("hii") == 20
+
+def test_namaste():
+    assert value("namaste") == 100
+
+if __name__ == "__main__":
+    main()
