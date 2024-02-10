ed2e35485b4292929f8dbbf39ef4d36752247411
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-10 10:15:08 +0000

    Sat, Feb 10, 2024, 3:45 PM +05:30

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -7,11 +7,11 @@ def value(greet):
     greet = greet.strip()
 
     if greet.startswith("hello"):
-        x="$100"
+        x="$0"
     elif greet.startswith("h"):
         x="$20"
     else:
-        x="$0"
+        x="$100"
     return x
 
 if __name__ == "__main__":
