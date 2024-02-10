c8715cae3218f8c5fe6aecfb54213d402439f066
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-10 11:53:36 +0000

    Sat, Feb 10, 2024, 5:23 PM +05:30

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -7,7 +7,32 @@ except:
     from fuel import gauge
 
 def test_prompt():
-    with pytest.raises(TypeError):
+    try:
         assert convert(3/4) == "75%"
+        assert convert(1/3) == "33%"
+        assert convert(2/3) == "67%"
+    except TypeError:
+        pass
+    except ZeroDivisionError:
+        pass
 
+def test_EandF():
+    try:
+        assert convert(0/100) == "E"
+        assert convert(1/100) == "E"
+        assert convert(100/100) == "F"
+        assert convert(99/100) == "F"
+    except TypeError:
+        pass
+    except ZeroDivisionError:
+        pass
+
+# def test_reprompt():
+#     try:
+#         assert convert(100/0) == "E"
+#         assert convert(1/100) == "E"
+#         assert convert(100/100) == "F"
+#         assert convert(99/100) == "F"
+#     except TypeError ZeroDivisionError:
+#         pass
 
