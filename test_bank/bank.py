ea805c9a04cc0dd88692ee9f72e8ed87ee8c19ff
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-10 10:27:55 +0000

    Sat, Feb 10, 2024, 3:57 PM +05:30

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -7,11 +7,11 @@ def value(greet):
     greet = greet.strip()
 
     if greet.startswith("hello"):
-        x="$0"
+        x=0
     elif greet.startswith("h"):
-        x="$20"
+        x=20
     else:
-        x="$100"
+        x=100
     return x
 
 # if __name__ == "__main__":
--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -10,22 +10,22 @@ except:
 
 
 def test_hello():
-    assert value("hello") == "$0"
+    assert value("hello") == 0
 def test_HELLO():
-    assert value("HELLO") == "$0"
+    assert value("HELLO") == 0
 
 
 def test_hii():
-    assert value("hii") == "$20"
+    assert value("hii") == 20
 
 def test_HII():
-    assert value("HII") == "$20"
+    assert value("HII") == 20
 
 
 def test_NAMASTE():
-    assert value("namaste") == "$100"
+    assert value("namaste") == 100
 def test_namaste():
-    assert value("NAMASTE") == "$100"
+    assert value("NAMASTE") == 100
 
 
 # if __name__ == "__main__":
