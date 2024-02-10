62e48daf71ae530e6870103a364003d222012137
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-10 12:02:38 +0000

    Sat, Feb 10, 2024, 5:32 PM +05:30

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -10,32 +10,36 @@ except:
     from fuel import gauge
 
 def test_prompt():
-    # try:
+    try:
         assert convert(3/4) == "75%"
         assert convert(1/3) == "33%"
         assert convert(2/3) == "67%"
-    # except TypeError:
-    #     pass
-    # except ZeroDivisionError:
-    #     pass
+    except TypeError:
+        pass
+    except ZeroDivisionError:
+        pass
 
 def test_EandF():
-    # try:
+    try:
         assert convert(0/100) == "E"
         assert convert(1/100) == "E"
         assert convert(100/100) == "F"
         assert convert(99/100) == "F"
-    # except TypeError:
-    #     pass
-    # except ZeroDivisionError:
-    #     pass
+    except TypeError:
+        pass
+    except ZeroDivisionError:
+        pass
 
-# def test_reprompt():
-#     try:
-#         assert convert(100/0) == "E"
-#         assert convert(1/100) == "E"
-#         assert convert(100/100) == "F"
-#         assert convert(99/100) == "F"
-#     except TypeError ZeroDivisionError:
-#         pass
+def test_reprompt():
+    try:
+        assert convert(100/0) is None
+        assert convert(10/3) is None
+        assert convert("three/four") is None
+        assert convert(1.5/4) is None
+        assert convert(3/5.5) is None
+        assert convert(5-10) is None
+    except TypeError:
+        pass
+    except ZeroDivisionError:
+        pass
 
