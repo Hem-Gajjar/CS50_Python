8a68e3375bbe1769d58654249fc04ed51dd9cdc6
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-10 10:08:36 +0000

    Sat, Feb 10, 2024, 3:38 PM +05:30

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -8,15 +8,15 @@ def main():
 
 def test_hello():
     assert value("Hello") == "$0"
-    print("done")
+
 
 def test_hii():
     assert value("hii") == "$20"
-    print("done")
+
 
 def test_namaste():
     assert value("namaste koi kaam") == "$100"
-    print("done")
+
 
 if __name__ == "__main__":
     main()
