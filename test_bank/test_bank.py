22936a5a001563a27bc41ae90addb85fb1e472c8
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-10 07:17:28 +0000

    Sat, Feb 10, 2024, 12:47 PM +05:30

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -10,11 +10,11 @@ def test_hello():
     assert value("Hello") == 0
     assert value("hello") == 0
 
-# def test_hii():
-#     assert value("hii") == 20
+def test_hii():
+    assert value("hii") == 20
 
 def test_namaste():
-    assert value("namaste") == 100
+    assert value("namaste koi kaam") == 100
 
 if __name__ == "__main__":
     main()
