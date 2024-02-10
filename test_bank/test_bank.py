3ed1d9a0b8fca820bdeba9fb1cabbce118a298ff
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-10 10:20:40 +0000

    Sat, Feb 10, 2024, 3:50 PM +05:30

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -10,15 +10,18 @@ except:
 
 
 def test_hello():
-    assert value("Hello") == "$0"
+    assert value("hello") == "$0"
+    assert value("HELLO") == "$0"
 
 
 def test_hii():
     assert value("hii") == "$20"
+    assert value("HII") == "$20"
 
 
 def test_namaste():
     assert value("namaste") == "$100"
+    assert value("NAMASTE") == "$100"
 
 
 # if __name__ == "__main__":
