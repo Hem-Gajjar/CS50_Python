a44678bf64c096bc91b4bd542f01512ee79b2321
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-10 06:54:44 +0000

    Sat, Feb 10, 2024, 12:24 PM +05:30

---
+++ test_bank/bank.py
@@ -0,0 +1,17 @@
+def main():
+    print(value("Hii"))
+
+def value(greet):
+
+    greet = greet.lower()
+    greet = greet.strip()
+    if greet.startswith("hello"):
+        x=0
+    elif greet.startswith("h"):
+        x=20
+    else:
+        x=100
+    return x
+
+if __name__ == "__main__":
+    main()
