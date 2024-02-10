83c68d2979bfcbbb7e5a9242ef597f177eefba53
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-10 11:54:57 +0000

    Sat, Feb 10, 2024, 5:24 PM +05:30

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,10 +1,10 @@
-import pytest
+
 try:
     from test_fuel.fuel import convert
-    from test_fuel.fuel import gauge
+
 except:
     from fuel import convert
-    from fuel import gauge
+
 
 def test_prompt():
     try:
