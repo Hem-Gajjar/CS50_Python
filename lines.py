22d9521ed1de2bbbaae3e458b6e7a78896940462
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-22 04:40:18 +0000

    Thu, Feb 22, 2024, 10:10 AM +05:30

--- lines.py
+++ lines.py
@@ -1,8 +1,20 @@
 import sys
 
 count = len(sys.argv)
+file_name = sys.argv[1]
+name,ext = file_name.split(".")
+
 
 if count < 2:
     sys.exit("Too few command-line arguments")
 elif count > 2:
     sys.exit("Too many command-line arguments")
+elif ext != py:
+    sys.exit("Not a Python file")
+
+
+try:
+    with open(file_name,"r") as file:
+        for line in file:
+            print(line)
+except FileNotFoundError:
