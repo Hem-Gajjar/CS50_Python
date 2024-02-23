c9ed1c3cd092cb3b5d911458a7e934177563c31f
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-23 12:02:36 +0000

    Fri, Feb 23, 2024, 5:32 PM +05:30

--- scourgify.py
+++ scourgify.py
@@ -1,8 +1,26 @@
 import sys
+import csv
 count = len(sys.argv)
 
 if count<3:
-    print("")
+    print("Too few command-line arguments")
 elif count>3:
-    print("")
-elif 
+    print("Too many command-line arguments")
+
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+
+students = []
+
+try:
+    with open(file1) as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            students.append({"name":row["name"],"house":row["house"]})
+
+    for student in students:
+        print(f"{student['name']},{student['house']}")
+
+        
+except FileNotFoundError:
+    sys.exit("Could not read",file1)
