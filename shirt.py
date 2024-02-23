12b0fa4d239eb13c92226b3d19fbeaec4cd87638
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-23 18:54:36 +0000

    Sat, Feb 24, 2024, 12:24 AM +05:30

--- shirt.py
+++ shirt.py
@@ -6,5 +6,15 @@ if count < 2:
     sys.exit("Too few command-line arguments")
 elif count > 2:
     sys.exit("Too many command-line arguments")
-elif count == 2:
-    
+
+filename1 = sys.argv[1]
+filename2 = sys.argv[2]
+
+name1,ext1 = filename1.split["."]
+name2,ext2 = filename2.split["."]
+
+ext1 = ext1.lower()
+ext2 = ext2.lower()
+
+if ext1 != "jpg" or ext1 != "jpeg" or ext1 != "png" or ext2 != "jpg" or ext2 != "jpeg" or ext2 != "png":
+    sys.exit()
