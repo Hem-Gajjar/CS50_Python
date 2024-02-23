a8296ae36fc96a888b5fcd679f55c743c6e91260
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-23 18:48:12 +0000

    Sat, Feb 24, 2024, 12:18 AM +05:30

--- shirt.py
+++ shirt.py
@@ -2,4 +2,8 @@ import sys
 
 count = len(sys.argv)
 
-if count != 2
+if count < 2:
+    sys.exit("Too few command-line arguments")
+elif count > 2:
+    sys.exit("Too many command-line arguments")
+elif 
