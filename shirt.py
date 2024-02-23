b9eb21cff33f7ebf554f1df6285cc84ee7d3a1bc
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-23 19:01:41 +0000

    Sat, Feb 24, 2024, 12:31 AM +05:30

--- shirt.py
+++ shirt.py
@@ -8,18 +8,24 @@ elif count > 3:
     sys.exit("Too many command-line arguments")
 
 filename1 = sys.argv[1]
+# print(filename1)
 filename2 = sys.argv[2]
+# print(filename2)
 try:
-    name1,ext1 = filename1.split["."]
-    name2,ext2 = filename2.split["."]
+    name1,ext1 = filename1.split(".")
+    name2,ext2 = filename2.split(".")
 except:
     sys.exit("Invalid input")
 
 ext1 = ext1.lower()
 ext2 = ext2.lower()
+print(ext1)
+print(ext2)
 
 
 if ext1 != "jpg" or ext1 != "jpeg" or ext1 != "png" or ext2 != "jpg" or ext2 != "jpeg" or ext2 != "png":
+    print("jjj")
     sys.exit()
+
 else:
     print("done")
