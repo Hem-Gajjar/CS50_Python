6d95a73389d652c018f47607926ede3b520abcd6
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-23 19:10:59 +0000

    Sat, Feb 24, 2024, 12:40 AM +05:30

--- shirt.py
+++ shirt.py
@@ -9,9 +9,8 @@ elif count > 3:
     sys.exit("Too many command-line arguments")
 
 filename1 = sys.argv[1]
-# print(filename1)
 filename2 = sys.argv[2]
-# print(filename2)
+
 try:
     name1,ext1 = filename1.split(".")
     name2,ext2 = filename2.split(".")
@@ -20,14 +19,18 @@ except:
 
 ext1 = ext1.lower()
 ext2 = ext2.lower()
-# print(ext1)
-# print(ext2)
 
 
 if ext1 == "jpg" or ext1 == "jpeg" or ext1 == "png" or ext2 == "jpg" or ext2 == "jpeg" or ext2 == "png":
     if ext1 != ext2:
         sys.exit("Input and output have different extensions")
     else:
+        images = []
+
+        for arg in sys.argv[1:]:
+            image = Image.open(arg)
+            images.append(image)
+
         
 
 else:
