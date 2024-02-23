efb2c719b795a918c353c1df5a67eea8c337565d
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-23 12:07:50 +0000

    Fri, Feb 23, 2024, 5:37 PM +05:30

--- scourgify.py
+++ scourgify.py
@@ -16,11 +16,18 @@ try:
     with open(file1) as file:
         reader = csv.DictReader(file)
         for row in reader:
-            students.append({"name":row["name"],"house":row["house"]})
+            students.append({"first":row["name"],"house":row["house"]})
 
     for student in students:
         print(f"{student['name']},{student['house']}")
 
-        
 except FileNotFoundError:
     sys.exit("Could not read",file1)
+
+
+try:
+    with open(file2,"a") as file:
+        writer= csv.DictWriter(file,fieldnames=["first","last","house"])
+        writer.writerow({"first":first})
+except FileNotFoundError:
+    sys.exit("Could not read",file2)
