b312083a41e1c23b1685e3938922e3940e51b846
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-02-28 07:04:18 +0000

    Wed, Feb 28, 2024, 12:34 PM +05:30

---
+++ numb3rs.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+def main():
+    ip = input("IPv4 Address:")
+    validate(ip)
+
+def validate(ip):
+    flag = False
+    if re.search(r"^.+[0-255]\.+[0-255]\.+[0-255]\.+[0-255]\.$",ip):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+
+if __name__ == "__main__":
+    main()
