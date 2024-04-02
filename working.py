94925aea78812034117529102ed52bb960667744
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-02 13:00:27 +0000

    Tue, Apr 2, 2024, 6:30 PM +05:30

--- working.py
+++ working.py
@@ -10,11 +10,12 @@ def convert(s):
     isCorrect = re.search(r"^(([0-9][0-2]*):*([0-5][0-9])*) ([A-P]M) to (([0-9][0-2]*):*([0-5][0-9])*) ([A-P]M)$",s)
     if isCorrect:
         pieces = isCorrect.groups()
-        if int(p)
+        if int(pieces[1])>12 or int(pieces[5])>12:
+            raise ValueError
         return pieces
     else:
         raise ValueError
 
-
+def new_format(hour,minute,am_pm)
 if __name__ == "__main__":
     main()
