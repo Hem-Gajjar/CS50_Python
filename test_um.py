71070dc4eae6d242b2f5c44f476db0eeedbb2faa
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-02 13:49:32 +0000

    Tue, Apr 2, 2024, 7:19 PM +05:30

---
+++ test_um.py
@@ -0,0 +1,17 @@
+from um import count
+
+def main():
+    test_ulcase()
+    test_uminword()
+    test_umalone()
+
+def test_ulcase():
+    assert count("Um, Thanks for the album")==1
+    assert count("Um, thanks, um...")==2
+def test_uminword():
+    assert count("yummy") == 0
+def test_umalone():
+    assert count("hi um iron man") == 1
+    assert count("um@") == 1
+if __name__ == "__main__":
+    main()
