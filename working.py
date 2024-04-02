d522d766c7289bd1028f010004951de0c4bc5bfd
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-02 08:00:20 +0000

    Tue, Apr 2, 2024, 1:30 PM +05:30

--- working.py
+++ working.py
@@ -7,14 +7,18 @@ def main():
 
 
 def convert(s):
-    if re.search(r"(([0-9].*)((:[0-9])*) [A-Z]+ to ([0-9].*)((:[0-9])*) [A-Z]+)",s):
-        get_groups = re.search(r"(([0-9].*)((:[0-9])*) [A-Z]+ to ([0-9].*)((:[0-9])*) [A-Z]+)",s)
+    if re.search(r"(([0-9].*)((:[0-9])*) ([A-Z]+) to ([0-9].*)((:[0-9])*) ([A-Z]+))",s):
+        get_groups = re.search(r"(([0-9].*)((:[0-9])*) ([A-Z]+) to ([0-9].*)((:[0-9])*) ([A-Z]+))",s)
         our_groups = get_groups.groups()
         print(our_groups)
         start = our_groups[1]
-        end = our_groups[4]
+        start_zone = our_groups[4]
+        end = our_groups[5]
+        end_zone = our_groups[8]
         print(start)
+        print(start_zone)
         print(end)
+        print(end_zone)
         
 
 if __name__ == "__main__":
