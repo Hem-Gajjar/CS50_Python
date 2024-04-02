5d7a85c98d3269c1c50129951a817324b21fd104
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-02 12:49:38 +0000

    Tue, Apr 2, 2024, 6:19 PM +05:30

--- working.py
+++ working.py
@@ -7,38 +7,12 @@ def main():
 
 
 def convert(s):
-    if re.search(r"(([0-9].*)((:[0-9])*) ([A-Z]+) to ([0-9].*)((:[0-9])*) ([A-Z]+))",s):
-        get_groups = re.search(r"(([0-9].*)((:[0-9])*) ([A-Z]+) to ([0-9].*)((:[0-9])*) ([A-Z]+))",s)
-        our_groups = get_groups.groups()
-        print(our_groups)
-        # final_start_hour,final_start_min,final_end_hour,final_end_min
-        try:
-            start = our_groups[1]
-            if re.search("([0-9]:[0-9]*)",start):
-                start_hour,start_min = start.split(":")
-                start_zone = our_groups[4]
-                if(start_zone=="AM"):
-                    final_start_hour = 0 + int(start_hour)
-                    final_start_min = start_min
-                else:
-                    final_start_hour = 12 + int(start_hour)
-                    final_start_min = start_min
-
-            end = our_groups[5]
-            if re.search("([0-9]:[0-9]*)",end):
-                end_hour,end_min = end.split(":")
-                end_zone = our_groups[8]
-                if(end_zone=="AM"):
-                    final_end_hour = 0 + int(end_hour)
-                    final_end_min = end_min
-                else:
-                    final_end_hour = 12 + int(end_hour)
-                    final_end_min = end_min
-        except:
-            pass
-        final = final_start_hour+":"+final_start_min+" to "+final_end_hour+":"+final_end_min
-        return (final)
-
+    is isCorrect = re.search(r"^(([0-9][0-2]*):*([0-5][0-9])*) ([A-P]M) to (([0-9][0-2]*):*([0-5][0-9])*) ([A-P]M)$",s)
+    if isCorrect:
+        pieces = isCorrectFormat.groups()
+        return pieces
+    else:
+        raise ValueError
 
 
 if __name__ == "__main__":
