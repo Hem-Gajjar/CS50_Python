3c777a8904095407627e26c5f275df9982ee77b1
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-02 07:14:16 +0000

    Tue, Apr 2, 2024, 12:44 PM +05:30

--- watch.py
+++ watch.py
@@ -7,11 +7,11 @@ def main():
 
 
 def parse(s):
-    re.search(r"<iframe(.)*><\/iframe>",s)
-    url_pattern = re.search(r"(http(s)*:\/\/(www\.)*youtube\.com\/embed\/([a-zA-Z0-9]+))",s)
-    if url_pattern:
-        get_pattern = url_pattern.groups()
-        return ("https://youtu.be/"+get_pattern[3])
-        # print(get_pattern)
+    if re.search(r"<iframe(.)*><\/iframe>",s):
+        url_pattern = re.search(r"(http(s)*:\/\/(www\.)*youtube\.com\/embed\/([a-zA-Z0-9]+))",s)
+        if url_pattern:
+            get_pattern = url_pattern.groups()
+            return ("https://youtu.be/"+get_pattern[3])
+
 if __name__ == "__main__":
     main()
