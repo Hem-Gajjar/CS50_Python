83be09b3a8a6b349484a1ff7551eb3ad00db0788
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-02 07:05:52 +0000

    Tue, Apr 2, 2024, 12:35 PM +05:30

--- watch.py
+++ watch.py
@@ -10,7 +10,8 @@ def parse(s):
     re.search(r"<iframe(.)*><\/iframe>",s)
         url_pattern = re.search("(http(s)*\:\/\/(www\.)*youtube\.com\/embed\/[a-z_A-Z_0-9]+)",s)
         if url_pattern:
-
+            get_pattern = url_pattern.groups()
+            return "https://youtu.be/"+get_pattern[3]
 
 if __name__ == "__main__":
     main()
