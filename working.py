8c945d9dd93f5e59ecee1f7bdce9c26588675975
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-02 07:54:44 +0000

    Tue, Apr 2, 2024, 1:24 PM +05:30

--- working.py
+++ working.py
@@ -10,7 +10,10 @@ def convert(s):
     if re.search(r"(([0-9].*)((:[0-9])*) [A-Z]+ to ([0-9].*)((:[0-9])*) [A-Z]+)",s):
         get_groups = re.search(r"(([0-9].*)((:[0-9])*) [A-Z]+ to ([0-9].*)((:[0-9])*) [A-Z]+)",s)
         our_groups = get_groups.groups()
-        print(our_groups)
+        start = our_groups[2]
+        end = our_groups[5]
+        print(start)
+        print(end)
 
 
 if __name__ == "__main__":
