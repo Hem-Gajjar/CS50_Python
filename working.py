9dba8f8812746ee31540f73b5365b2bf99137407
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-02 13:07:47 +0000

    Tue, Apr 2, 2024, 6:37 PM +05:30

--- working.py
+++ working.py
@@ -12,10 +12,29 @@ def convert(s):
         pieces = isCorrect.groups()
         if int(pieces[1])>12 or int(pieces[5])>12:
             raise ValueError
-        return pieces
+        first_time = new_format(pieces[1],pieces[2],pieces[3])
+        second_time = new_format(pieces[5],pieces[6],pieces[7])
+        return first_time + " to " + second_time
     else:
         raise ValueError
 
-def new_format(hour,minute,am_pm)
+def new_format(hour,minute,am_pm):
+    if am_pm =="PM":
+        if int(hour) = 12
+        new_hour = 12
+        else:
+            new_hour = int(hour) + 12
+    else:
+        if int(hour) ==12:
+            new_hour = 0
+        else:
+            new_hour = int(hour)
+
+    if minute == None:
+        new_minute = ":00"
+        new_time = f"{new_hour:02}"+new_minute
+    else
+        new_time = f"{new_hour:02}"+":"+minute
+    return new_time
 if __name__ == "__main__":
     main()
