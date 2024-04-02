1c5a058d4da7e731352247161260124276c9b04a
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-02 06:24:36 +0000

    Tue, Apr 2, 2024, 11:54 AM +05:30

--- test_numb3rs.py
+++ test_numb3rs.py
@@ -17,8 +17,10 @@ def test_format():
 def test_range():
     assert validate(r"255.255.255.255")==True
     assert validate(r"0.0.0.0")==True
-    assert validate(r"50.2.2.2")==False
-    assert validate(r"2.2.2222.2")==False
+    assert validate(r"500.2.2.2")==False
+    assert validate(r"2.500.2.2")==False
+    assert validate(r"2.2.500.2")==False
+    assert validate(r"2.2.2.500")==False
 
 
 if __name__ == "__main__":
