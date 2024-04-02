623ba709ccec89525b7a05992fe5b0d610c7cdfa
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-02 08:05:32 +0000

    Tue, Apr 2, 2024, 1:35 PM +05:30

--- working.py
+++ working.py
@@ -11,15 +11,22 @@ def convert(s):
         get_groups = re.search(r"(([0-9].*)((:[0-9])*) ([A-Z]+) to ([0-9].*)((:[0-9])*) ([A-Z]+))",s)
         our_groups = get_groups.groups()
         print(our_groups)
-        start = our_groups[1]
-        start_hour,start_min = start.split(":")
-        start_zone = our_groups[4]
-        end = our_groups[5]
-        end_hour,end_min = end.split(":")
-        end_zone = our_groups[8]
+        try:
+            start = our_groups[1]
+            start_hour,start_min = start.split(":")
+            start_zone = our_groups[4]
+            end = our_groups[5]
+            end_hour,end_min = end.split(":")
+            end_zone = our_groups[8]
+        except:
+            pass
         print(start)
+        print(start_hour)
+        print(start_min)
         print(start_zone)
         print(end)
+        print(end_hour)
+        print(end_min)
         print(end_zone)
         final_time=""
 
