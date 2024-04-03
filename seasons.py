ca9c6e2e4fe3a43c14e5d5b58bcc90eebc0842a8
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-03 18:22:38 +0000

    Wed, Apr 3, 2024, 11:52 PM +05:30

--- seasons.py
+++ seasons.py
@@ -1,27 +1,14 @@
 from datetime import date
 import sys
-import re
-import inflect
-p = inflect.engine()
 
 def main():
-    birth_date = input("Date of birth: ")
     try:
-        year,month,day = check_dob(birth_date)
-    except:
+        year,month,day = input("Date of Birth").split("-")
+    except ValueError:
         sys.exit("Invalid Date")
-    date_of_birth = date(int(year),int(month),int(day))
-    date_of_today = date.today()
 
-    diff = date_of_today - date_of_birth
-    total_minutes = diff.days * 24*60
-    output = p.number_to_words(total_minutes, andword="")
-    print(output.capitalize() + " minutes")
+...
 
-def check_dob(dob):
-    if re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$",dob):
-        year,month,day = dob.split("-")
-        return year,month,day
 
 if __name__ == "__main__":
     main()
