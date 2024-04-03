e55924b22e43f1fc4562a5b2fc481c1310fb3076
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-03 19:08:24 +0000

    Thu, Apr 4, 2024, 12:38 AM +05:30

--- jar.py
+++ jar.py
@@ -1,7 +1,9 @@
 class Jar:
     def __init__(self, capacity=12):
-        ...
-
+        if capacity <0:
+            raise ValueError("Wrong capacity")
+        self.capacity = capacity
+        self._size
     def __str__(self):
         ...
 
