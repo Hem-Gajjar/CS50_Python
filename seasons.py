31a082364107e250c8a00404b70250ff817da828
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-03 18:24:33 +0000

    Wed, Apr 3, 2024, 11:54 PM +05:30

--- seasons.py
+++ seasons.py
@@ -6,9 +6,10 @@ def main():
         year,month,day = input("Date of Birth").split("-")
     except ValueError:
         sys.exit("Invalid Date")
+    minutes_lived(year,month,day))
 
-...
-
-
+def minutes_lived(year,month,day):
+    dt = date(int(year),int(month),int(day))
+    print(dt)
 if __name__ == "__main__":
     main()
