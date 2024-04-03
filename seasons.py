b46d28e80e75b72cfbd931ca0dfe445f0253ef75
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-03 18:48:07 +0000

    Thu, Apr 4, 2024, 12:18 AM +05:30

--- seasons.py
+++ seasons.py
@@ -1,24 +1,25 @@
 from datetime import date
 import inflect
 import sys
+import operator
+
 p = inflect.engine()
+
+
 def main():
     try:
-        year,month,day = input("Date of Birth").split("-")
+        dob = input("Date of Birth: ")
+        # date.fromisoformat(dob) will check whether dob is a valid date or not
+        difference = operator.sub(date.today(), date.fromisoformat(dob))
+        print(convert(difference.days))
     except ValueError:
-        sys.exit("Invalid Date")
-    print(minutes_lived(year,month,day))
+        sys.exit("Invalid date")
 
-def minutes_lived(year,month,day):
-    try:
-        dt = date(int(year),int(month),int(day))
-    except ValueError:
-        return "Invalid Date"
 
-    tday = date.today()
-    diff = tday - dt
-    minutes = int(diff.total_seconds()/60)
-    msg = p.number_to_words(minutes, andword="") + " minutes"
-    return msg.capitalize()
+def convert(time):
+    minutes = time * 24 * 60
+    return f"{(p.number_to_words(minutes, andword='')).capitalize()} minutes"
+
+
 if __name__ == "__main__":
     main()
--- test_seasons.py
+++ test_seasons.py
@@ -1,16 +1,6 @@
-from seasons import minutes_lived
+from seasons import convert
 
 
-def main():
-    test_1()
-    test_2()
-
-def test_1():
-    assert minutes_lived(2003,5,17) == "Ten million, nine hundred eighty-four thousand, three hundred twenty minutes"
-    assert minutes_lived(2000,2,1) == "Twelve million, seven hundred thirteen thousand, seven hundred sixty minutes"
-    # assert minutes_lived(1999,01,01) == "Five hundred twenty-five thousand, six hundred minutes"
-
-def test_2():
-    assert minutes_lived(23,1,2000) == "Invalid Date"
-if __name__ == "__main__":
-    main()
+def test_date():
+    assert convert(10477) == "Fifteen million, eighty-six thousand, eight hundred eighty minutes"
+    assert convert(365) == "Five hundred twenty-five thousand, six hundred minutes"
