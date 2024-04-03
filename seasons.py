69fa5fac75e9ea1632f56e4d0185bbc5da24953e
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-03 18:43:12 +0000

    Thu, Apr 4, 2024, 12:13 AM +05:30

--- seasons.py
+++ seasons.py
@@ -1,24 +1,28 @@
-from datetime import date
+from datetime import timedelta
 import inflect
 import sys
-p = inflect.engine()
+
 def main():
-    try:
-        year,month,day = input("Date of Birth").split("-")
-    except ValueError:
-        sys.exit("Invalid Date")
-    print(minutes_lived(year,month,day))
+    print(convert(input("Date of Birth: ")))
 
-def minutes_lived(year,month,day):
-    try:
-        dt = date(int(year),int(month),int(day))
-    except ValueError:
-        return "Invalid Date"
 
-    tday = date.today()
-    diff = tday - dt
-    minutes = int(diff.total_seconds()/60)
-    msg = p.number_to_words(minutes, andword="") + " minutes"
-    return msg.capitalize()
+def convert(x):
+    q = inflect.engine()
+    try:
+        years, months, days = x.split("-")
+        years = int(years)
+        months = int(months)
+        days = int(days)
+        dob = date(years, months, days)
+        today = date.today()
+        difference = today - dob
+        min = round(difference.total_seconds()/60)
+        words = q.number_to_words(min, andword="").capitalize() + " minutes"
+        return words
+    except ValueError:
+        sys.exit("Date must be inputted in YYYY-MM-DD format.")
+
+
+
 if __name__ == "__main__":
     main()
--- test_seasons.py
+++ test_seasons.py
@@ -1,16 +1,12 @@
-from seasons import minutes_lived
+from seasons import convert
+import pytest
 
-
-def main():
-    test_1()
-    test_2()
-
-def test_1():
-    assert minutes_lived(2003,5,17) == "Ten million, nine hundred eighty-four thousand, three hundred twenty minutes"
-    assert minutes_lived(2000,2,1) == "Twelve million, seven hundred thirteen thousand, seven hundred sixty minutes"
-    assert minutes_lived(1999,01,01) == "Five hundred twenty-five thousand, six hundred minutes"
-
-def test_2():
-    assert minutes_lived(23,1,2000) == "Invalid Date"
-if __name__ == "__main__":
-    main()
+def test_seasons():
+    assert convert("2022-09-10") == "Five hundred twenty-five thousand, six hundred minutes"
+    assert convert("2021-09-10") == "One million, fifty-one thousand, two hundred minutes"
+    with pytest.raises(SystemExit):
+        convert("sdfs-sd-sd")
+    with pytest.raises(SystemExit):
+        convert("20220910")
+    with pytest.raises(SystemExit):
+        convert("cat")
