f5455de7e93e4756812831bc1eb6f4642d62ed7c
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-03 14:56:33 +0000

    Wed, Apr 3, 2024, 8:26 PM +05:30

--- seasons.py
+++ seasons.py
@@ -1,14 +1,20 @@
 from datetime import date
+import sys
+import re
 
 
 def main():
     birth_date = input("Date of birth: ")
+    try:
+        year,month,day = check_dob(birth_date)
+        print(year,month,day)
+    except:
+        sys.exit("Invalid Date")
 
-
-def check_dob(dob)
+def check_dob(dob):
     if re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$",dob):
-        year,month,day = dob.split()
-
+        year,month,day = dob.split("-")
+        return year,month,day
 
 if __name__ == "__main__":
     main()
