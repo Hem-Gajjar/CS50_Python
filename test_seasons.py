8a7e639b3d90975bd931b7a699cb323cf3f106f5
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-03 18:50:39 +0000

    Thu, Apr 4, 2024, 12:20 AM +05:30

--- test_seasons.py
+++ test_seasons.py
@@ -1,6 +1,10 @@
 from seasons import convert
-
+def main():
+    test_date()
 
 def test_date():
     assert convert(10477) == "Fifteen million, eighty-six thousand, eight hundred eighty minutes"
     assert convert(365) == "Five hundred twenty-five thousand, six hundred minutes"
+
+if __name__ == "__main__":
+    main()
