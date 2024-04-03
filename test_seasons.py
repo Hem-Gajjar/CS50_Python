66542b76c1e3ef06b9a03dc7b72abcdae5a2a7eb
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-03 18:38:10 +0000

    Thu, Apr 4, 2024, 12:08 AM +05:30

--- test_seasons.py
+++ test_seasons.py
@@ -6,8 +6,8 @@ def main():
     test_2()
 
 def test_1():
-    assert minutes_lived(2003,5,17) == "Ten million, fifty-eight thousand, four hundred minutes"
-    assert minutes_lived(2000,2,1) == "Eleven million, seven hundred wighty-seven thousand, eight hundred forty minutes"
+    assert minutes_lived(2003,5,17) == "Ten million, nine hundred eighty-four thousand, three hundred twenty minutes"
+    assert minutes_lived(2000,2,1) == "Twelve million, seven hundred thirteen thousand, seven hundred sixty minutes"
 
 def test_2():
     assert minutes_lived(23,1,2000) == "Invalid Date"
