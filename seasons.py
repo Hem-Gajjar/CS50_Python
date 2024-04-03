57b7278d3148886827aac34c381db8b61ce895e8
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-03 14:48:29 +0000

    Wed, Apr 3, 2024, 8:18 PM +05:30

--- seasons.py
+++ seasons.py
@@ -2,10 +2,12 @@ from datetime import date
 
 
 def main():
-    birth_date = 
+    birth_date = input("Date of birth: ")
 
 
-...
+def check_dob(dob)
+    if re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$",dob):
+        year,month,day = dob.split()
 
 
 if __name__ == "__main__":
