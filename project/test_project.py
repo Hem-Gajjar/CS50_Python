8ee8e119accf881aceaccf033631f0cb88312a6f
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-04 09:32:24 +0000

    Thu, Apr 4, 2024, 3:02 PM +05:30

--- project/test_project.py
+++ project/test_project.py
@@ -3,12 +3,16 @@ from project import select_by_genre
 from project import select_by_actor
 
 def test_select_by_year():
-    assert 
+    assert select_by_year("1971") == True
+    assert select_by_year("1972") == False
 
 
 def test_select_by_genre():
-    ...
+    assert select_by_genre("Drama") == True
+    assert select_by_genre("Nothing") == False
 
 
 def test_select_by_actor():
-    ...
+    assert select_by_actor("Upendra Trivedi") == True
+    assert select_by_actor("Arvind Trivedi") == True
+
