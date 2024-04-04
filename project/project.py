60142d1d52d4b9ddb67a60ed5c88a6e4c05c6e81
Hem Gajjar <109205993+Hem-Gajjar@users.noreply.github.com>
2024-04-04 05:17:04 +0000

    Thu, Apr 4, 2024, 10:47 AM +05:30

--- project/project.py
+++ project/project.py
@@ -1,6 +1,15 @@
 def main():
     choice = 0
-    
+    print(f"Select Choice\n(1) Select Movie By Year\n(2) Select Movie By Genre\n()")
+    match term:
+    case pattern-1:
+         action-1
+    case pattern-2:
+         action-2
+    case pattern-3:
+         action-3
+    case _:
+        action-default
 
 
 def function_1():
